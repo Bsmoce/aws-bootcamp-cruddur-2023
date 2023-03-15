ee3272316d365ba5b9ba838b54c56e24fdef79f6
Bassam <Bassamasar@duck.com>
2023-03-15 16:53:49 +0000

    Setting up PROD DB & SG Rule

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,13 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Bassam', 'BSM' ,'MOCK');
+  ('Bassam', 'BSM101' ,'MOCK'),
+  ('Andrew Brown', 'andrewbrown' ,'MOCK');
+  
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'BSM101' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
